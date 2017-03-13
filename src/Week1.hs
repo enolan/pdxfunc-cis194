d174c1f04277e37d32f56591aef95f4d21481d24
Echo Nolan <echo@echonolan.net>
2017-03-13 10:34:57 -0700

    Comments

--- src/Week1.hs
+++ src/Week1.hs
@@ -32,21 +32,9 @@ validateCC n = let
   sumDigits = sum flattened
   in sumDigits `rem` 10 == 0
 
-
--- Validate credit card numbers
--- validateCC :: Integer -> Bool
--- validateCC n = False -- YOUR CODE GOES HERE
-
 -- Towers of Hanoi
 type Peg = String
 type Move = (Peg, Peg)
--- hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
--- hanoi n src tgt tmp = [] -- YOUR CODE GOES HERE
-
--- Towers of Hanoi with an extra peg. This is an optional exercise. It is more
--- an algorithms problem than a Haskell problem.
--- hanoi2 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
--- hanoi2 n src tgt tmp1 tmp2 = [] -- YOUR CODE GOES HERE
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _   _   _   = []
