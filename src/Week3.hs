47aa531da5e88f7043fbf192b70d5e8de4c8c93b
Echo Nolan <echo@echonolan.net>
2017-04-27 12:17:05 -0700

    Skeleton for week 3

---
+++ src/Week3.hs
@@ -0,0 +1,10 @@
+module Week3 where
+
+skips :: [a] -> [[a]]
+skips xs = [] -- YOUR CODE GOES HERE
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima i = [] -- YOUR CODE GOES HERE
+
+histogram :: [Integer] -> String
+histogram is = "" -- YOUR CODE GOES HERE
